e8d9af58e8d98f1178b62c506456ec5e80eae7c8
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-24 23:01:00 -0400

    add tests folder to components, set up the first test

---
+++ testing/src/components/__tests__/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from '../App';
+
+describe('the App component', () => {
+  it('knows math!', () => {
+    expect(2 + 2).toEqual(4);
+  });
+});
