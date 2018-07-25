2fe264cfc40bc94045bdf8e0a4a2a789c4b4075b
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-25 15:40:18 -0400

    write out first render test for CommentBox

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -4,7 +4,15 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 describe('the App component', () => {
-  it('knows math!', () => {
-    expect(2 + 2).toEqual(4);
+  it('renders the comment box properly', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<App />, div);
+
+    // inspect the div and see if CommentBox
+    // is rendered
+    expect(div.innerHTML).toContain('And this is the comment box!!');
+
+    ReactDOM.unmountComponentAtNode(div);
   });
 });
