9bd2d42f869e74767a33a41d77889ff05061c03f
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-28 13:07:52 -0400

    refactor App tests with beforeEach

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -6,18 +6,20 @@ import App from '../App';
 import CommentBox from '../CommentBox';
 import CommentList from '../CommentList';
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
 describe('the App component', () => {
   it('renders the comment box properly', () => {
-    const wrapper = shallow(<App />);
-
     // expect to show just one instance of CommentBox
     // inside the App Component
     expect(wrapper.find(CommentBox).length).toEqual(1);
   });
 
   it('renders the comment list properly', () => {
-    const wrapper = shallow(<App />);
-
     expect(wrapper.find(CommentList).length).toEqual(1);
   });
 });
