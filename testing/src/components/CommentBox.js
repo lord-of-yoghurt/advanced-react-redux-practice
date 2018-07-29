1c914ab5c744b9e650c5bc4b9ef7febf8b50378f
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-29 22:07:49 +0200

    wire up editing the comment form and capturing user input

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  state = {
+    comment: ''
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      comment: e.target.value
+    });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: call action creator
+    // TODO: save comment
+
+    this.setState({ comment: '' });
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onFormSubmit}>
         <h4>Say something clever!</h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment} />
         <div>
           <button>Submit</button>
         </div>
