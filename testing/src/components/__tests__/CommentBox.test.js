04e9f5a35dcefc4bfc7dbeb50c247f2fd6383b09
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-30 23:46:02 +0200

    use full render from enzyme and make some assertions for CommentBox

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { mount } from 'enzyme';
 
 import CommentBox from '../CommentBox';
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = mount(<CommentBox />);
+});
+
 describe('CommentBox component', () => {
-  it('has a text area and a button', () => {
-    
+  it('has a text area and a button', () => {expect(wrapper.find('textarea').length).toEqual(1);
+    expect(wrapper.find('button').length).toEqual(1);
   });
 });
