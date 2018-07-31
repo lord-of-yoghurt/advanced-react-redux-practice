17b37916bb28acef406a237846f084c327a6b791
David Ordovskiy <david.ordovsky@gmail.com>
2018-07-31 10:45:22 +0200

    add test to check user input in CommentBox text area

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -17,4 +17,24 @@ describe('CommentBox component', () => {
   it('has a text area and a button', () => {expect(wrapper.find('textarea').length).toEqual(1);
     expect(wrapper.find('button').length).toEqual(1);
   });
+
+  it('allows users to type in the text area', () => {
+    // find the textarea element and simulate an event
+    // of the 'change' sort (even names come from HTML.)
+    // the second argument is a fake event object, which
+    // has a `target` property carrying a value (since
+    // in the real thing the value is at event.target.value)
+    wrapper.find('textarea').simulate('change', {
+      target: { value: 'i love peaches' }
+    });
+
+    // force the component to update immediately (setState
+    // is asynchronous, and we don't wanna wait for it)
+    wrapper.update();
+
+    // for the expectation, find the textarea once again
+    // and look at its 'value' property - it should be
+    // updated with what we gave it above
+    expect(wrapper.find('textarea').prop('value')).toEqual('i love peaches');
+  });
 });
