0abe498efdcad35670f2739802afe5ef730369cb
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-03 00:24:52 +0200

    create saveComment action and wire it up to redux side

--- testing/src/reducers/comments.js
+++ testing/src/reducers/comments.js
@@ -1,5 +1,9 @@
-export default (state = {}, action) => {
+import { SAVE_COMMENT } from '../actions/types';
+
+export default (state = [], action) => {
   switch (action.type) {
+    case SAVE_COMMENT:
+      return [...state, action.payload]
     default:
       return state;
   }
