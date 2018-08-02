137c25758931f234c4a01730566fbf6d52092146
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-03 00:20:12 +0200

    wire up redux side to react app

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import reducers from './reducers';
 import App from './components/App';
 
+const store = createStore(reducers, {});
+
+const jsx = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
 ReactDOM.render(
-  <App />,
+  jsx,
   document.getElementById('root')
 );
