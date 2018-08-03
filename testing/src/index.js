0629c42d19145b97f29577b49595ddf2baa8719f
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-03 09:59:27 +0200

    create generic Root component to use with redux in both dev & test

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
-import reducers from './reducers';
+import Root from './Root';
 import App from './components/App';
 
-const store = createStore(reducers, {});
-
 const jsx = (
-  <Provider store={store}>
+  <Root>
     <App />
-  </Provider>
-)
+  </Root>
+);
 
 ReactDOM.render(
   jsx,
