11fa3290cd13eaa1b954ac079f32e551a9e7feaa
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-04 09:56:45 +0200

    set up test file for comments reducer, write first test

---
+++ testing/src/reducers/__tests__/comments.test.js
@@ -0,0 +1,15 @@
+import commentsReducer from '../comments';
+import { SAVE_COMMENT } from '../../actions/types';
+
+describe('Comments reducer', () => {
+  it('handles actions of the SAVE_COMMENT type', () => {
+    const action = {
+      type: SAVE_COMMENT,
+      payload: 'Testing rules!'
+    };
+
+    const newState = commentsReducer([], action);
+
+    expect(newState).toEqual(['Testing rules!']);
+  });
+});
