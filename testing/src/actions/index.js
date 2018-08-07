7a47d954132366f6566dea70a3b5927b9b05ca55
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-07 15:42:16 +0200

    set up action creator for fetching comments

--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -1,8 +1,21 @@
-import { SAVE_COMMENT } from './types';
+import axios from 'axios';
+
+import { SAVE_COMMENT, FETCH_COMMENTS } from './types';
+
+const API_URL = 'http://jsonplaceholder.typicode.com/posts';
 
 export const saveComment = (comment) => {
   return {
     type: SAVE_COMMENT,
     payload: comment
+  };
+};
+
+export const fetchComments = () => {
+  const response = axios.get(API_URL);
+
+  return {
+    type: FETCH_COMMENTS,
+    payload: response
   };
 };
