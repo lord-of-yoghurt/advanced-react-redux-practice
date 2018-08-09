65471049f9a725b262bf7a54b867771a35efa622
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-09 16:13:20 +0200

    fix CommentBox test, start putting together integration test

--- testing/src/__tests__/integrations.test.js
+++ testing/src/__tests__/integrations.test.js
@@ -7,6 +7,11 @@ import App from '../components/App';
 describe('App integration', () => {
   it('can fetch and display a list of comments', () => {
     // render the app
+    const wrapper = mount(
+      <Root>
+        <App />
+      </Root>
+    );
 
     // find and click fetchComments
 
--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -20,7 +20,7 @@ afterEach(() => {
 
 describe('CommentBox component', () => {
   it('has a text area and a button', () => {expect(wrapper.find('textarea').length).toEqual(1);
-    expect(wrapper.find('button').length).toEqual(1);
+    expect(wrapper.find('button').length).toEqual(2);
   });
 });
 
