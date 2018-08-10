dc0c1e85ce85f17d81ad21f9bd95ca47ab65c704
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-10 17:34:00 +0200

    wire up auth reducer and start creating header

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import CommentList from './CommentList';
 import CommentBox from './CommentBox';
 
-const App = () => (
-  <div>
-    <Route path="/post" component={CommentBox} />
-    <Route path="/" exact={true} component={CommentList} />
-  </div>
-);
+class App extends Component {
+  renderButton() {
+
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a comment</Link>
+        </li>
+        <li>
+          {this.renderButton()}
+        </li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact={true} component={CommentList} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- testing/src/reducers/index.js
+++ testing/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import commentsReducer from './comments';
+import authReducer from './auth';
 
 export default combineReducers({
+  auth: authReducer,
   comments: commentsReducer
 });
