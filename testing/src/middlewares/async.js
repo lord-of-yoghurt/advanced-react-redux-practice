dad528b1604a3aab55daed4281f6efaa7edef38d
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-13 05:11:34 +0200

    start putting together async middleware logic

--- testing/src/middlewares/async.js
+++ testing/src/middlewares/async.js
@@ -1,5 +1,9 @@
-// next is a reference to the next middleware
-// in the chain
 export default ({ dispatch }) => (next) => (action) => {
-  
+  // does the action have a promise on its payload property?
+  // if so, wait for it to resolve
+
+  // otherwise, send to the next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
 };
