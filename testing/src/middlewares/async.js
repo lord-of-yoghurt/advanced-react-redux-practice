c12a5bdb41110cdc0cac0ce34a0a12fcf822d9e5
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-13 07:11:57 +0200

    finish up the async middleware

--- testing/src/middlewares/async.js
+++ testing/src/middlewares/async.js
@@ -1,9 +1,23 @@
 export default ({ dispatch }) => (next) => (action) => {
   // does the action have a promise on its payload property?
-  // if so, wait for it to resolve
-
-  // otherwise, send to the next middleware
+  // if not, send it to the next middleware
   if (!action.payload || !action.payload.then) {
     return next(action);
   }
+
+  // get the data off the promise (have it resolve),
+  // create a new action with that data and dispatch it.
+  // the dispatch is what starts the entire process of
+  // taking an action and calling middlewares with it.
+  // effectively, it feeds the action to the top of the
+  // middleware chain and all the reducers.
+
+  // here, we overwrite the payload by calling `then`
+  // of the current payload and creating a new action
+  // in the callback. afterwards, we call dispatch with
+  // the new action. 
+  action.payload.then((res) => {
+    const newAction = { ...action, payload: res };
+    dispatch(newAction);
+  });
 };
