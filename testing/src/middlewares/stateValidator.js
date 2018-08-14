220262b578a249428b2b3b222f7d1387873e997c
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-14 14:04:30 +0800

    emit warnings whenever stateValidator middleware detects invalid data

--- testing/src/middlewares/stateValidator.js
+++ testing/src/middlewares/stateValidator.js
@@ -4,8 +4,11 @@ import stateSchema from './stateSchema';
 
 export default ({ dispatch, getState }) => (next) => (action) => {
   // right off the bat, feed the action to the reducers
-  // and back to the middlewares
+  // and back to the middlewares, so that we have some state
   next(action);
 
-  console.log(tv4.validate(getState(), stateSchema));
+  // if the validation fails
+  if (!tv4.validate(getState(), stateSchema)) {
+    console.warn('Detecting an invalid data type in your state! Please debug.');
+  }
 };
