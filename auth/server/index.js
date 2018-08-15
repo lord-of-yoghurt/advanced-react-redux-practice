2ba7f2f2278707bd30cabea519ad033bf1028f9d
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-16 07:39:18 +0800

    connect to the database, have options for localhost and mlab

--- auth/server/index.js
+++ auth/server/index.js
@@ -5,7 +5,16 @@ const express = require('express'),
       http = require('http'),
       bodyParser = require('body-parser'),
       morgan = require('morgan'),
+      mongoose = require('mongoose'),
       app = express();
+
+/*
+ * DB Setup
+ */
+const keys = require('./settings/keys');
+// use if offline
+// mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true });
+mongoose.connect(`${keys.dbLink}`, { useNewUrlParser: true });
 
 /*
  * Route imports
