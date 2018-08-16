d7cf9a7cbe244ce9248a3cdd7add158b8210670f
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-16 17:28:17 +0800

    small changes to user creation functionality

--- auth/server/controllers/authController.js
+++ auth/server/controllers/authController.js
@@ -1,17 +1,19 @@
 const User = require('../models/User');
 
 exports.signUp = (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  const email = req.body.email,
+        password = req.body.password;
 
   User.findOne({ email })
     .then((user) => {
       if (user) return res.status(422).send({ error: 'User with this email already exists!' });
 
       const newUser = new User({ email, password });
-      
+
       newUser.save()
-        .then(() => res.json(newUser))
+        .then(() => res.json(
+          { message: `User successfully created: ${newUser.email}` }
+        ))
         .catch((e) => next(e));
     })
     .catch((e) => next(e));
