0957b63932339c4083838d917c7706992154aaa6
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-17 15:48:40 +0800

    stub out a route to connect to passport and authenticate user

--- auth/server/router.js
+++ auth/server/router.js
@@ -1,5 +1,18 @@
+const passport = require('passport');
+
 const Auth = require('./controllers/authController');
+const passportConfig = require('./services/passport');
+
+const requireAuth = passport.authenticate(
+  'jwt',
+  // no cookies needed when using JWT
+  { session: false }
+);
 
 module.exports = (app) => {
+  app.get('/', requireAuth, (req, res) => {
+    res.send({ message: 'oh hai :D' });
+  });
+
   app.post('/signup', Auth.signUp);
 };
