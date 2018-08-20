eb58fc83726c7c977b9c6a15cac1b8d6842e547e
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-20 13:08:07 +0800

    add redux-thunk to the app

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 
@@ -10,7 +11,11 @@ import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 
-const store = createStore(reducers, {});
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(reduxThunk)
+);
 
 const jsx = (
   <Provider store={store}>
