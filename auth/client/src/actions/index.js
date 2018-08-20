510c2b3e7e79f361a4049113938a522894d1b8eb
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-20 13:25:23 +0800

    start writing out the sign-up action creator

---
+++ auth/client/src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+import { AUTH_USER } from './types';
+
+const BASE_URL = 'http://localhost:4000';
+
+// redux-thunk allows us to return a function from
+// the action creator - this function will be called
+// with a dispatch argument, which is itself a function
+// that acts as something of a funnel to feed our
+// actions down to middlewares and reducers. dispatch
+// can be called as many times as we need. additionally,
+// if there's an async request returning a promise, we can
+// wait for the promise to resolve and put the dispatch
+// call on the `then` callback. in short, redux-thunk
+// lets us take full control over the dispatch function.
+
+export const signup = (formProps) => (dispatch) => {
+  axios.post(`${BASE_URL}/signup`, formProps);
+};
