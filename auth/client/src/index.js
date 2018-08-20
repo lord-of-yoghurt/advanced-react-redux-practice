f8db57cae4f6debd963af549bc430d2b28e7193d
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-20 12:23:10 +0800

    make the scaffold for the sign up form and wire it up

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
 
 const jsx = (
   <BrowserRouter>
     <App>
       <Route path="/" exact component={Welcome} />
+      <Route path="/signup" component={Signup} />
     </App>
   </BrowserRouter>
 );
