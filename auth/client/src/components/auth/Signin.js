fff1e53c4921201d0a6830a109c1685354c26997
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-21 09:20:55 +0800

    create and wire up Signin component

---
+++ auth/client/src/components/auth/Signin.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions';
+
+// TODO: refactor this and put the form in a separate, reusable component
+
+class Signin extends Component {
+  onSubmit = (formProps) => {
+    this.props.signin(formProps, () => {
+      this.props.history.push('/feature');
+    });
+  };
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <fieldset>
+          <label>Email</label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <div>
+          {this.props.errorMessage}
+        </div>
+        <button>Whoosh!</button>
+      </form>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  errorMessage: state.auth.errorMessage
+});
+
+// use compose when there's more than one HOC
+// we're hooking up our component to (in this case,
+// it's connect and reduxForm)
+export default compose(
+  connect(mapStateToProps, actions),
+  reduxForm({ form: 'signin' })
+)(Signin);
--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -10,6 +10,7 @@ import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
+import Signin from './components/auth/Signin';
 import Signout from './components/auth/Signout';
 import Feature from './components/Feature';
 
@@ -28,6 +29,7 @@ const jsx = (
         <Route path="/" exact component={Welcome} />
         <Route path="/signup" component={Signup} />
         <Route path="/signout" component={Signout} />
+        <Route path="/signin" component={Signin} />
         <Route path="/feature" component={Feature} />
       </App>
     </BrowserRouter>
