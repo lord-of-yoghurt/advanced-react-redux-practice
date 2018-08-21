d993c04590eb1094d1737c0dcb31d3d13e74a489
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-21 11:27:09 +0800

    render header links based on auth status

--- auth/client/src/components/Header.js
+++ auth/client/src/components/Header.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.signedIn) {
+      return (
+        <div>
+          <Link to="/signout">Sign Out</Link>
+          <Link to="/feature">Feature!</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/signin">Sign In</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div>
         <Link to="/">Auth App</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to="/feature">Feature!</Link>
+        {this.renderLinks()}
       </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  signedIn: state.auth.authenticated
+});
+
+export default connect(mapStateToProps)(Header);
