f0191ee1914d888d5c192c47ccdff7696f6666f1
David Ordovskiy <david.ordovsky@gmail.com>
2018-08-21 11:33:07 +0800

    add some very basic styling to the header

--- auth/client/src/components/Header.js
+++ auth/client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import '../styles/HeaderStyle.css';
+
 class Header extends Component {
   renderLinks() {
     if (this.props.signedIn) {
@@ -23,7 +25,7 @@ class Header extends Component {
 
   render() {
     return (
-      <div>
+      <div className="header">
         <Link to="/">Auth App</Link>
         {this.renderLinks()}
       </div>
